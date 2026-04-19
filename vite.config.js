b3f7d3c2ad95ba080d7d1465fad3fb827e15e072
Muhammad Hasbi <hasbi@akasia.id>
2026-04-19 14:04:33 +0800

    refactor: update code style by replacing double quotes with single quotes in route configuration and build settings

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const isProduction = mode === "production";
-
   return {
     base: "/",
     plugins: [
