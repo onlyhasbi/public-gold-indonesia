35dc038226ca4d6f663c8f200bea1cb7a1347309
Muhammad Hasbi <hasbi@akasia.id>
2026-04-19 14:06:26 +0800

    chore: update vite config and reformat generated route tree file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     base: "/",
     plugins: [
